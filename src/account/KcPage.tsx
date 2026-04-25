067838e7c4d61406802d03861570d0bcb5975202
ARTYKBAS <assel.artykbay@bcchub.kz>
2026-04-25 14:01:33 +0500

    little fixes in ui

--- src/account/KcPage.tsx
+++ src/account/KcPage.tsx
@@ -7,5 +7,13 @@ const KcAccountUi = lazy(() => import("./KcAccountUi"));
 export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
-    return <KcAccountUiLoader kcContext={kcContext} KcAccountUi={KcAccountUi} />;
+    // Keycloak v25 doesn't include referrerName in the account context unless an app
+    // explicitly configures a referrer. The loader treats undefined values as missing
+    // polyfills and logs an error, so we default to "" to signal "no referrer".
+    const kcContextPolyfilled = {
+        ...kcContext,
+        referrerName: kcContext.referrerName ?? ""
+    };
+
+    return <KcAccountUiLoader kcContext={kcContextPolyfilled} KcAccountUi={KcAccountUi} />;
 }
